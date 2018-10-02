1d6216d27614d6c7c1eccddd810bae15c8f794ca
Ubuntu <ubuntu@ip-172-31-88-118.ec2.internal>
2018-10-02 04:02:52 +0000

    first

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# zkLeaderSelection" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/durai145/zkLeaderSelection.git
+git push -u origin master
