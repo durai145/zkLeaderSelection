a5dffa78dba71cbcd4aa352468d3a8a5f4050e4d
durai <durai145@live.in>
2018-10-03 15:22:55 -0400

    check exits node and create

--- leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/main/StaticNodeSetup.java
+++ leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/main/StaticNodeSetup.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
@@ -13,25 +15,34 @@ import org.apache.zookeeper.ZooKeeper;
 
 public class StaticNodeSetup {
 
+	private static final String PATH_SEPRATOR = null;
 	private static ZooKeeper zooKeeper;
 
-	public static void main(String [] argv) {
-		
-		if (argv.length ==0) {
+	public static void main(String[] argv) {
+
+		if (argv.length == 0) {
 			usage();
 		}
-		
+
 		File file = new File(argv[0]);
 		String line = "";
 
 		BufferedReader br;
 		try {
+			zooKeeper = new ZooKeeper("18.210.40.41:2181,35.175.71.81:2181,35.175.71.81:2181", 3000, null);
+
 			br = new BufferedReader(new FileReader(file));
 			while ((line = br.readLine()) != null) {
 				String[] strArray = line.split("=");
-				zooKeeper = new ZooKeeper("18.210.40.41:2181,35.175.71.81:2181,35.175.71.81:2181", 3000, null);
+				String zNodPath = strArray[0];
+				List<String> allRootPath = parseZNodePath(zNodPath);
+				// checkNode is exit
+				allRootPath.forEach(node -> {
+					checkZNodeORCreate(node);
+				});
 				zooKeeper.create(strArray[0], strArray[1].getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,12 +53,27 @@ public class StaticNodeSetup {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+	}
+
+	private static void checkZNodeORCreate(String node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static List<String> parseZNodePath(String zNodPath) {
+		List<String> nodes = new ArrayList<>();
+		String[] dirs = zNodPath.split("/");
+		String parent = "";
+		for (String dir : dirs) {
+			parent = parent + PATH_SEPRATOR + dir;
+			nodes.add(parent);
+		}
+		return nodes;
 	}
 
 	private static void usage() {
 		System.out.println("StaticNodeSetup <filename>");
-		
+
 	}
 }
