1a5f3db1863cbf3b6df7737de22a690bd8606f9b
Saurabh Gigras <Saurabh Gigras@10.253.0.191>
2018-10-03 19:40:57 -0400

    ClientAgent changes for monitoring and creating dynamic node

--- leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/main/ClientAgent.java
+++ leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/main/ClientAgent.java
@@ -15,6 +15,7 @@ public class ClientAgent implements Runnable {
 	ZooKeeper zookeeper;
 	String hostname;
 	private static final String ELECTED_SERVER_PATH = "/election/server";
+	private String myCurrentDynamicNodePath;
 
 	public ClientAgent(String url) {
 		InetAddress ip;
@@ -36,23 +37,22 @@ public class ClientAgent implements Runnable {
 	public boolean findAndCreateZnode() {
 		Stat parentStat;
 		try {
-			
-			//checkServer is up 
-			String serverName=checkServer() ;
+
+			// checkServer is up
+			String serverName = checkServer();
 			boolean exists = false;
 			parentStat = zookeeper.exists("/static", false);
 			if (parentStat != null) {
 				List<String> children = zookeeper.getChildren("/static", false);
-				children.forEach(child -> {
-					 exists = child.equals(this.hostname);
+				for(String child : children) {
+					exists = child.equals(this.hostname);
 					if (exists) {
 						createDynamicName(serverName);
 						break;
-						}
-							
-				});
-				if(!exists)
-				{
+					}
+
+				}
+				if (!exists) {
 					System.out.println("Exiting :: Static config doesnt exist for the host " + this.hostname);
 				}
 			}
@@ -64,43 +64,108 @@ public class ClientAgent implements Runnable {
 		return false;
 	}
 
-	private void createDynamicName(String serverName) {
-		String dynamicServerPath = "/dynamic/"+serverName;
-		Stat dynamicNodeStat = zookeeper.exists(dynamicServerPath, false);
-		if (dynamicNodeStat != null) {
-			String path = zookeeper.create(dynamicServerPath+this.hostname, null, Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-			if (path != null) {
-			    String ClientAppPath = zookeeper.create(path+, data, acl, createMode) 
+	private void createDynamicName(String serverName) throws KeeperException {
+		try {
+			String dynamicServerPath = "/dynamic/" + serverName;
+			Stat dynamicNodeStat = zookeeper.exists(dynamicServerPath, false);
+			if (dynamicNodeStat != null) {
+				String path = zookeeper.create(dynamicServerPath + this.hostname, null, Ids.OPEN_ACL_UNSAFE,
+						CreateMode.EPHEMERAL);
+				if (path != null) {
+					String ClientAppPath = zookeeper.create(path + "/G4CMONITOR", null, Ids.OPEN_ACL_UNSAFE,
+							CreateMode.EPHEMERAL);
+					if (ClientAppPath != null) {
+						String ClientQueuePath = ClientAppPath + "/" + "INSTQID";
+						Stat nodeStat = zookeeper.exists(ClientQueuePath, false);
+						if (nodeStat == null) {
+
+							// get data from static config
+							String staticNodePath = "/static/" + this.hostname + "/G4CMONITOR/INSTQID";
+							Stat staticNodestat = null;
+							byte[] data = zookeeper.getData(staticNodePath, false, staticNodestat);
+							String QueuePath;
+
+							QueuePath = zookeeper.create(ClientQueuePath, data, Ids.OPEN_ACL_UNSAFE,
+									CreateMode.EPHEMERAL);
+							if (QueuePath == null) {
+								System.out.println("Failed to create Znode = " + QueuePath);
+
+							} else {
+								this.myCurrentDynamicNodePath = QueuePath;
+								System.out.println("Dynamic Client Znode:: " + QueuePath + "created successfully");
+							}
+
+						}
+					} else {
+						System.out.println("Failed to create Znode = " + path + "/G4CMONITOR");
+					}
+
+				} else {
+					System.out.println("Failed to create Znode = " + dynamicServerPath + this.hostname);
+				}
+			} else {
+				System.out.println(" Znode = " + dynamicServerPath + "doesn't exist");
 			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+
 	}
 
-	private String checkServer() {
-		
-		byte[] serverName;
-		Stat serverStat = zookeeper.exists(ELECTED_SERVER_PATH, true);
-		if (serverStat != null)
-		{
-			serverName = zookeeper.getData(ELECTED_SERVER_PATH, true, serverStat);
+	private String checkServer() throws KeeperException, InterruptedException {
+
+		byte[] serverName = null;
+		Stat serverStat;
+		try {
+			serverStat = zookeeper.exists(ELECTED_SERVER_PATH, true);
+			if (serverStat != null) {
+				serverName = zookeeper.getData(ELECTED_SERVER_PATH, true, serverStat);
+			}
+
+			return new String(serverName);
+		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
 		}
-		if (serverName != null)
-		return serverName.toString();
+
 	}
 
 	@Override
 	public void run() {
+		while (true) {
+			if (this.myCurrentDynamicNodePath != null) {
+				Stat stat = null;
+				byte[] data;
+				try {
+					data = this.zookeeper.getData(this.myCurrentDynamicNodePath, true, stat);
+					System.out.println("Client " + this.hostname + "Processing Queues " + data.toString());
+				} catch (KeeperException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		}
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public static void main(String argv[]) {
-		
+
 		String url = argv[0];
 		ClientAgent client = new ClientAgent(url);
-		client.Start();
-		
-	}
-	}
+		Thread t = new Thread(client);
+		t.start();
 
+	}
 }
