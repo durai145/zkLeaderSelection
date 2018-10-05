2ea2c12ef8d2ff1d91c05d986385f0a61ec07f3b
Saurabh Gigras <Saurabh Gigras@10.253.0.191>
2018-10-04 20:05:26 -0400

    static path and data changes

--- leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/nodes/TestStaticNode.java
+++ leader-election/src/main/java/com/sts/allprogtutorials/zk/leaderelection/nodes/TestStaticNode.java
@@ -23,13 +23,19 @@ public class TestStaticNode {
 	}
 	public static void getStaticNodeList() {
 
-		List<String> staticNodes = zooKeeperService.getChildren("/static", true);
+		List<String> staticNodes = zooKeeperService.getChildren("/static/G4CMONITOR", true);
 		System.out.println("ZnodePathChildren :: " + staticNodes);
 		List<ConfigData> staticConfig = new ArrayList<>();
 		staticNodes.forEach(zpath -> {
+			
+			
 			try {
 				System.out.println("Node:: " + zpath);
-				byte[] data = zooKeeperService.getZooKeeper().getData("/static/" + zpath, false, null);
+				String temp = "/static/" + zpath;
+				
+				ConfigData.zNodeInfo nodeTemp = new ConfigData.zNodeInfo("static","G4CMONITOR",zpath);
+				System.out.println("nodTemp :: " + nodeTemp);
+				byte[] data = zooKeeperService.getZooKeeper().getData(nodeTemp.getStaticPath(), false, null);
 				String strData = new String(data);
 				System.out.println("Node:: " + strData);
 				ConfigData zConfigData = gson.fromJson(strData, ConfigData.class);
